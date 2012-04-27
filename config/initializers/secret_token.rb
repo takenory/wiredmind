92434dc58e75e22d2c0adac3a2df8cdcdd3de910
takenory <takenory@gmail.com>
2012-04-27 12:42:21 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wiredmind::Application.config.secret_token = '288b66975b5404c0a4f50db43dca99456268fe00170962b381a3bbca0a791db7faca904260230120ab5c2e39bf9fe9f37ed4ef9ec198475a11152d90d5bc1a87'
