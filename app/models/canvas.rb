9f1393d7988fe8382f5638d18d734a21fd1d4a70
takenory <takenory@gmail.com>
2012-05-01 10:14:32 +0900

    spacified association in 'Canvas' model

--- app/models/canvas.rb
+++ app/models/canvas.rb
@@ -1,3 +1,4 @@
 class Canvas < ActiveRecord::Base
+  has_many :minds
   attr_accessible :name
 end
--- spec/models/canvas_spec.rb
+++ spec/models/canvas_spec.rb
@@ -1,5 +1,21 @@
 require 'spec_helper'
 
 describe Canvas do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'association' do
+    before do
+      @canvas = Canvas.create(name: 'canvas')
+      @mind_count = 5
+      @minds = @mind_count.times.collect do |i|
+                 mind = Mind.create(name: "mind_#{i}")
+                 mind.canvas_id = @canvas.id
+                 mind.save
+               end 
+    end
+    it 'Canvas.#minds returns instance of Array whitch include instance of "Mind"' do
+      @canvas.minds.should have(@mind_count).items
+      @canvas.minds.each do |mind|
+        mind.should be_instance_of Mind
+      end
+    end
+  end
 end
