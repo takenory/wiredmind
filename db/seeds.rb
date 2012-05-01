539977ad9e486c6767b31b2a9f988fa88d001167
takenory <takenory@gmail.com>
2012-05-01 10:45:51 +0900

    add minds seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,17 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 Canvas.destroy_all
+Mind.destroy_all
 canvas = 5.times.collect {|i| Canvas.create(name: "canvas#{i}") }
-
+positions = [{top: 100, left: 100}, 
+             {top: 100, left: 100}, 
+             {top: 200, left: 200}, 
+             {top: 200, left: 200}, 
+             {top: 300, left: 300}]
+canvas.each_with_index do |canvas, i|
+  positions.each_with_index do |position, j|
+    canvas.minds << Mind.create(name: "mind_#{i}_#{j}",
+                                top: position[:top],
+                                left: position[:left])
+  end
+end
